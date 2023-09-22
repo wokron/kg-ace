a293bd15a59ba05c3c91d70dd1b94f7e18b90469
wokron <stringcatwok@gmail.com>
2023-09-22 18:08:57 +0800

    feat: KGClassifier state save and load

--- model.py
+++ model.py
@@ -6,9 +6,13 @@ import torch
 
 class KGClassifier(TextClassifier):
     def __init__(
-        self, embeddings: DocumentEmbeddings, label_type: str, **classifierargs
+        self,
+        embeddings: DocumentEmbeddings,
+        label_type: str,
+        selection: torch.Tensor = torch.Tensor([1, 2, 5, 6, 7, 8, 9]),
+        **classifierargs
     ):
-        self.selection = None  # for embedding selection
+        self.selection: torch.Tensor = selection  # for embedding selection
         super().__init__(embeddings, label_type, **classifierargs)
 
     def _get_embedding_for_data_point(
@@ -17,3 +21,18 @@ class KGClassifier(TextClassifier):
         embedding_names = self.embeddings.get_names()
         embeddings = prediction_data_point.get_each_embedding(embedding_names)
         return torch.cat(embeddings, dim=0)  # this is prepared for selection
+
+    def _get_state_dict(self):
+        model_state = {
+            **super()._get_state_dict(),
+            "selection": self.selection,
+        }
+        return model_state
+
+    @classmethod
+    def _init_model_with_state_dict(cls, state, **kwargs):
+        return super()._init_model_with_state_dict(
+            state,
+            selection=state.get("selection"),
+            **kwargs,
+        )
